7a27baac45fbeaba223ea57d25bb964e66744738
DuroCodes <davidwright13503@gmail.com>
2026-02-12 11:25:25 -0500

    feat: make dragging work for background of the content

--- src/mainview/App.tsx
+++ src/mainview/App.tsx
@@ -1,7 +1,17 @@
 export default function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold text-white">Hello World</h1>
+    <div className="h-screen text-white bg-red-500 electrobun-webkit-app-region-drag">
+      <div
+        className="flex items-center justify-center h-full electrobun-webkit-app-region-no-drag"
+        onMouseDown={(e) => {
+          // makes dragging only for the background rather than the content inside
+          if (e.target !== e.currentTarget) e.stopPropagation();
+        }}
+      >
+        <h1 className="w-5/6 h-5/6 rounded-full bg-green-300 text-center flex items-center justify-center">
+          <span className="text-xl font-bold text-black">hello world</span>
+        </h1>
+      </div>
     </div>
   );
 }
